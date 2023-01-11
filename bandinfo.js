9cc908bc737cd142749e537b8758495fd5f0697f
alsauce <2998687+alsauce@users.noreply.github.com>
2023-01-11 10:29:05 -0600

    Create bandinfo.js

---
+++ bandinfo.js
@@ -0,0 +1,4 @@
+var bandInfo = "Nightwish,symphonic power,,male+female clean vocals not my thing,https://open.spotify.com/playlist/37i9dQZF1DZ06evO1C7dIs?si=c5664441334d4471,,,,,,,,,|" +
+"Insomnium,melodic death,,seen favorite,https://open.spotify.com/playlist/37i9dQZF1DZ06evO21Vjqz?si=d39d39304ec443a9,,,,,,,,,|" +
+"Test day 1,melodic death,M,seen favorite,https://open.spotify.com/playlist/37i9dQZF1DZ06evO21Vjqz?si=d39d39304ec443a9,,,,,,,,,|" +
+"Test day 4_v2,melodic black,Y,seen really fun,https://open.spotify.com/playlist/37i9dQZF1DZ06evO4xvbnh?si=345952d04a0847a8,,,,,,,,,";
