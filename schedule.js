ae82e1799d5bede90a79217b8ac50db336eff476
alsauce <2998687+alsauce@users.noreply.github.com>
2023-01-11 10:27:51 -0600

    Create schedule.js

---
+++ schedule.js
@@ -0,0 +1,76 @@
+startTimeDay1 = new Date(2023, 0, 30, 16, 0, 0, 0);
+startHourDay1 = 16;
+
+eventsDay1 = [
+    {
+       "eventName": "Test day 1",
+       "eventLocation": "PD",
+       "eventStartDate": "30",
+       "eventEndDate": "30",
+       "eventStartTime": "5:15|PM",
+       "eventEndTime": "6:00|PM"
+   },
+   {
+      "eventName": "Test day 4_v2",
+      "eventInfo": "Day 4 Info",
+      "eventLocation": "RT",
+      "eventDay": "4",
+      "eventStartDate": "2",
+      "eventEndDate": "2",
+      "eventStartTime": "12:45|PM",
+      "eventEndTime": "1:45|PM"
+   }
+];
+startTimeDay2 = new Date(2023, 0, 31, 10, 0, 0, 0);
+
+eventsDay2 = [
+    {
+       "eventName": "Test day 2",
+       "eventInfo": "1st band usually",
+       "eventLocation": "PD",
+       "eventDay": "2",
+       "eventStartDate": "31",
+       "eventEndDate": "31",
+       "eventStartTime": "10:15|AM",
+       "eventEndTime": "11:00|AM"
+   }
+];
+
+eventsDay3 = [
+    {
+       "eventName": "Test day 1",
+       "eventInfo": "1st band usually",
+       "eventLocation": "PD",
+       "eventDay": "1",
+       "eventStartDate": "30",
+       "eventEndDate": "30",
+       "eventStartTime": "5:15|PM",
+       "eventEndTime": "6:00|PM"
+   }
+];
+
+eventsDay4 = [
+    {
+       "eventName": "Test day 1",
+       "eventInfo": "1st band usually",
+       "eventLocation": "PD",
+       "eventDay": "1",
+       "eventStartDate": "30",
+       "eventEndDate": "30",
+       "eventStartTime": "5:15|PM",
+       "eventEndTime": "6:00|PM"
+   }
+];
+
+eventsDay5 = [
+    {
+       "eventName": "Test day 1",
+       "eventInfo": "1st band usually",
+       "eventLocation": "PD",
+       "eventDay": "1",
+       "eventStartDate": "30",
+       "eventEndDate": "30",
+       "eventStartTime": "5:15|PM",
+       "eventEndTime": "6:00|PM"
+   }
+];
