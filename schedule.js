dd8a5251aa13792002a6d67d6751ad22eacf38cd
alsauce <2998687+alsauce@users.noreply.github.com>
2023-01-12 13:49:48 -0600

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,108 +1,93 @@
-startTimeDay1 = new Date(2023, 0, 30, 16, 0, 0, 0);
-startHourDay1 = 16;
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
-eventsDay1 = [
+<head>
+    <title>Alpha</title>
+
+    <script type="text/javascript" src="schedule.js?version=3"></script>
+    <script type="text/javascript" src="bandinfo.js?version=3"></script>
+    <script type="text/javascript">
+
+
+    function drawAlpha()
     {
-       "eventName": "BTest day 1",
-       "eventLocation": "PD",
-       "eventStartDate": "30",
-       "eventEndDate": "30",
-       "eventStartTime": "5:15|PM",
-       "eventEndTime": "6:00|PM"
-   },
-   {
-      "eventName": "Test day 1 midnight",
-      "eventLocation": "RT",
-      "eventStartDate": "30",
-      "eventEndDate": "31",
-      "eventStartTime": "11:45|PM",
-      "eventEndTime": "12:45|AM"
-   }
-];
-startTimeDay2 = new Date(2023, 0, 31, 10, 0, 0, 0);
-startHourDay2 = 10;
+        var topDiv = document.getElementById('topDiv');
+        labelWidth = Math.floor(window.innerWidth / 4.2) + "px";
+        let allEvents = [];
+        for (let i = 0; i < eventsDay1.length; i++) {
+            event = eventsDay1[i];
+            theText = event.eventName + "\nDay1\n" +event.eventStartTime + "-" + event.eventEndTime; 
+            allEvents.push(theText);
+        }
 
-eventsDay2 = [
-    {
-       "eventName": "Test day 2",
-       "eventLocation": "PD",
-       "eventStartDate": "31",
-       "eventEndDate": "31",
-       "eventStartTime": "10:15|AM",
-       "eventEndTime": "11:00|AM"
-   },
-   {
-      "eventName": "Test day 2 midnight",
-      "eventLocation": "RT",
-      "eventStartDate": "31",
-      "eventEndDate": "1",
-      "eventStartTime": "11:05|PM",
-      "eventEndTime": "12:00|AM"
-   },
-   {
-      "eventName": "Test day 2 #3",
-      "eventLocation": "RT",
-      "eventStartDate": "1",
-      "eventEndDate": "1",
-      "eventStartTime": "12:05|AM",
-      "eventEndTime": "1:00|AM"
-   },
-   {
-      "eventName": "DTest day 2 4",
-      "eventLocation": "RT",
-      "eventStartDate": "1",
-      "eventEndDate": "1",
-      "eventStartTime": "12:05|AM",
-      "eventEndTime": "1:00|AM"
-   },
-   {
-      "eventName": "Test day 2 5",
-      "eventLocation": "RT",
-      "eventStartDate": "1",
-      "eventEndDate": "1",
-      "eventStartTime": "12:05|AM",
-      "eventEndTime": "1:00|AM"
-   }
-];
+        for (let i = 0; i < eventsDay2.length; i++) {
+            event = eventsDay2[i];
+            theText = event.eventName + "\nDay2\n" +event.eventStartTime + "-" + event.eventEndTime; 
+            allEvents.push(theText);
+        }
 
-startTimeDay3 = new Date(2023, 1, 1, 10, 0, 0, 0);
-startHourDay3 = 10;
+        for (let i = 0; i < eventsDay3.length; i++) {
+            event = eventsDay3[i];
+            theText = event.eventName + "\nDay3\n" +event.eventStartTime + "-" + event.eventEndTime; 
+            allEvents.push(theText);
+        }
 
-eventsDay3 = [
-    {
-       "eventName": "CTest day 3",
-       "eventLocation": "PD",
-       "eventStartDate": "1",
-       "eventEndDate": "1",
-       "eventStartTime": "5:15|PM",
-       "eventEndTime": "6:00|PM"
-   }
-];
+        for (let i = 0; i < eventsDay4.length; i++) {
+            event = eventsDay4[i];
+            theText = event.eventName + "\nDay4\n" +event.eventStartTime + "-" + event.eventEndTime; 
+            allEvents.push(theText);
+        }
 
-startTimeDay4 = new Date(2023, 1, 2, 10, 0, 0, 0);
-startHourDay4 = 10;
+        for (let i = 0; i < eventsDay5.length; i++) {
+            event = eventsDay5[i];
+            theText = event.eventName + "\nDay5\n" +event.eventStartTime + "-" + event.eventEndTime; 
+            allEvents.push(theText);
+        }
 
-eventsDay4 = [
-    {
-       "eventName": "Test day 4",
-       "eventLocation": "PD",
-       "eventStartDate": "2",
-       "eventEndDate": "2",
-       "eventStartTime": "5:15|PM",
-       "eventEndTime": "6:00|PM"
-   }
-];
+        allEvents.sort();
 
-startTimeDay5 = new Date(2023, 1, 3, 10, 0, 0, 0);
-startHourDay5 = 10;
+        for (let i = 0; i < allEvents.length; i++) {
+            event = allEvents[i];
+            var label = document.createElement("textarea");
+            label.style.backgroundColor='lightgrey';
+            label.style.width=labelWidth;
+            label.style.height="200px";
+            label.style.fontSize="25px";
+            label.innerHTML = event;
+            topDiv.appendChild(label);
+        }
 
-eventsDay5 = [
-    {
-       "eventName": "ATest day 5",
-       "eventLocation": "PD",
-       "eventStartDate": "3",
-       "eventEndDate": "3",
-       "eventStartTime": "5:15|PM",
-       "eventEndTime": "6:00|PM"
-   }
-];
+    }
+
+    </script>
+
+</head>
+<body onload="drawAlpha();">
+    <style>
+        body {
+            margin: 0;
+        }
+
+        .buttonsss {
+            border: none;
+            padding: 15px 32px;
+            text-align: center;
+            color: black;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 60px;
+            margin: 4px 2px;
+            cursor: pointer;
+            background-color: gold;
+            border-radius: 12px;
+            height: 200px;
+            box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+        }
+    </style>
+
+<div id="topDiv">
+</div>
+
+</body>
+
+</html>
