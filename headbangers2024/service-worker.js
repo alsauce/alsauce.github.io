2ffa052b606b79862a9e675b512c0a5dccfaf12a
alsauce <2998687+alsauce@users.noreply.github.com>
2024-05-01 23:50:53 -0500

    reverting service worker

--- headbangers2024/service-worker.js
+++ headbangers2024/service-worker.js
@@ -1,22 +1,28 @@
 // Define the cache name and files to cache
-const CACHE_NAME = 'v7';
+const CACHE_NAME = 'headbangersboat_2024_v1';
+const urlsToCache = [
+    '/headbangers2024/',
+    '/headbangers2024/style.css',
+    '/headbangers2024/common.js',
+    '/headbangers2024/index.html',
+    '/headbangers2024/images/',
+    '/headbangers2024/images/dog.png',
+    '/headbangers2024/images/dude.png',
+    '/headbangers2024/images/hands.png',
+    '/headbangers2024/images/pinklady.png',
+    '/headbangers2024/images/search.png'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll([
-      '/headbangers2024/',
-      '/headbangers2024/style.css',
-      '/headbangers2024/common.js',
-      '/headbangers2024/index.html',
-      '/headbangers2024/images/',
-      '/headbangers2024/images/dog.png',
-      '/headbangers2024/images/dude.png',
-      '/headbangers2024/images/hands.png',
-      '/headbangers2024/images/pinklady.png',
-      '/headbangers2024/images/search.png'
-    ])),
-  );
-});
+
+// On install, cache the static resources
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      (async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(urlsToCache);
+      })()
+    );
+  });
 
 // delete old caches on activate
 self.addEventListener("activate", (event) => {
@@ -35,20 +41,20 @@ self.addEventListener("activate", (event) => {
     );
   });
 
-// Serve cached content when offline
 self.addEventListener('fetch', event => {
-
-event.respondWith(
-  (async () => {
+  event.respondWith(
+    (async () => {
       const cache = await caches.open(CACHE_NAME);
       const cachedResponse = await cache.match(event.request.url);
-
+      
       if (cachedResponse) {
+        // Return the cached response if it's available.
         return cachedResponse;
       }
 
+      // If cache miss, fetch from network
       return fetch(event.request).then(function(response) {
-        
+        // Cache the fetched response for future use
         cache.put(event.request.url, response.clone());
         return response;
 
@@ -56,3 +62,5 @@ event.respondWith(
     })()
   );
 });
+
+
