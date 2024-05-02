8d0437aa863e40965109d7f2f37da6a98d334607
alsauce <2998687+alsauce@users.noreply.github.com>
2024-05-01 23:33:58 -0500

    fix service worker

--- headbangers2024/service-worker.js
+++ headbangers2024/service-worker.js
@@ -1,27 +1,22 @@
 // Define the cache name and files to cache
 const CACHE_NAME = 'v7';
-const urlsToCache = [
-    '/headbangers2024/',
-    '/headbangers2024/style.css',
-    '/headbangers2024/common.js',
-    '/headbangers2024/index.html',
-    '/headbangers2024/images/',
-    '/headbangers2024/images/dog.png',
-    '/headbangers2024/images/dude.png',
-    '/headbangers2024/images/hands.png',
-    '/headbangers2024/images/pinklady.png',
-    '/headbangers2024/images/search.png'
-];
 
-// On install, cache the static resources
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-      (async () => {
-        const cache = await caches.open(CACHE_NAME);
-        cache.addAll(urlsToCache);
-      })()
-    );
-  });
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll([
+      '/headbangers2024/',
+      '/headbangers2024/style.css',
+      '/headbangers2024/common.js',
+      '/headbangers2024/index.html',
+      '/headbangers2024/images/',
+      '/headbangers2024/images/dog.png',
+      '/headbangers2024/images/dude.png',
+      '/headbangers2024/images/hands.png',
+      '/headbangers2024/images/pinklady.png',
+      '/headbangers2024/images/search.png'
+    ])),
+  );
+});
 
 // delete old caches on activate
 self.addEventListener("activate", (event) => {
@@ -40,16 +35,24 @@ self.addEventListener("activate", (event) => {
     );
   });
 
-self.addEventListener('fetch', (event) => {
-    console.log(event.request.url);
-    event.respondWith(
-      caches.match(event.request.url).then((response) => response || fetch(event.request).then(function(response) {
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+
+event.respondWith(
+  (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request.url);
+
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then(function(response) {
+        
         cache.put(event.request.url, response.clone());
         return response;
-      })),
-    );
-  });
 
-
-
-
+      });
+    })()
+  );
+});
