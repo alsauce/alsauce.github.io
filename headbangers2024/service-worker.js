a0e354ca992ffa99a1be3c3bffff572930daf64f
alsauce <2998687+alsauce@users.noreply.github.com>
2024-05-01 23:07:28 -0500

    trying manifest

---
+++ headbangers2024/service-worker.js
@@ -0,0 +1,55 @@
+// Define the cache name and files to cache
+const CACHE_NAME = 'v7';
+const urlsToCache = [
+    '/headbangers2024/',
+    '/headbangers2024/style.css',
+    '/headbangers2024/common.js',
+    '/headbangers2024/index.html',
+    '/headbangers2024/images/',
+    '/headbangers2024/images/dog.png',
+    '/headbangers2024/images/dude.png',
+    '/headbangers2024/images/hands.png',
+    '/headbangers2024/images/pinklady.png',
+    '/headbangers2024/images/search.png'
+];
+
+// On install, cache the static resources
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      (async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(urlsToCache);
+      })()
+    );
+  });
+
+// delete old caches on activate
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+      (async () => {
+        const names = await caches.keys();
+        await Promise.all(
+          names.map((name) => {
+            if (name !== CACHE_NAME) {
+              return caches.delete(name);
+            }
+          })
+        );
+        await clients.claim();
+      })()
+    );
+  });
+
+self.addEventListener('fetch', (event) => {
+    console.log(event.request.url);
+    event.respondWith(
+      caches.match(event.request.url).then((response) => response || fetch(event.request).then(function(response) {
+        cache.put(event.request.url, response.clone());
+        return response;
+      })),
+    );
+  });
+
+
+
+
