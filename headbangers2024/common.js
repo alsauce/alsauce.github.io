7cadadddb7d8106a162c612426dc5c0a94490563
alsauce <2998687+alsauce@users.noreply.github.com>
2024-08-16 00:08:40 -0500

    fixed top, cleaner data

--- headbangers2024/common.js
+++ headbangers2024/common.js
@@ -40,14 +40,14 @@ export let year = 2024;
 
 export let schedule = [
 "\
-PO, Oct, 28,  5:30, PM, Oct, 28,  6:00, PM, Toast/Photo \n\
+PO, Oct, 28,  5:30, PM, Oct, 28,  6:00, PM, Toast/ Photo \n\
 PO, Oct, 28,  6:00, PM, Oct, 28,  7:00, PM, Exodus \n\
 PO, Oct, 28,  8:30, PM, Oct, 28,  9:30, PM, Dethklok \n\
 PO, Oct, 28, 11:00, PM, Oct, 29, 12:00, AM, Chimaira \n\
 ST, Oct, 28,  7:15, PM, Oct, 28,  8:15, PM, Napalm Death \n\
 ST, Oct, 28,  9:45, PM, Oct, 28, 10:45, PM, Soulfly \n\
 ST, Oct, 29, 12:15, AM, Oct, 29,  1:15, AM, Poison the Well \n\
-AT, Oct, 28,  3:00, PM, Oct, 28,  4:00, PM, Corrosion of Conformity \n\
+AT, Oct, 28,  3:00, PM, Oct, 28,  4:00, PM, C.O.C. \n\
 AT, Oct, 28,  7:15, PM, Oct, 28,  8:15, PM, Bleeding Through \n\
 AT, Oct, 28,  9:45, PM, Oct, 28, 10:45, PM, Frozen Soul \n\
 AT, Oct, 29,  1:15, AM, Oct, 29,  2:15, AM, After the Burial \n\
@@ -60,23 +60,23 @@ SC, Oct, 28,  9:45, PM, Oct, 28, 10:45, PM, Unearth \
 PO, Oct, 29,  4:30, PM, Oct, 29,  5:30, PM, Napalm Death \n\
 PO, Oct, 29,  6:45, PM, Oct, 29,  7:45, PM, Bleeding Through \n\
 PO, Oct, 29,  9:30, PM, Oct, 29, 10:45, PM, Lamb of God \n\
-ST, Oct, 29, 11:30, AM, Oct, 29,  12:30, PM, Bingo w/ Riki/Leah \n\
+ST, Oct, 29, 11:30, AM, Oct, 29,  12:30, PM, Bingo w/ Riki/ Leah \n\
 ST, Oct, 29,  2:45, PM, Oct, 29,  3:45, PM, Drum-off w/ Art Cruz \n\
 ST, Oct, 29,  5:15, PM, Oct, 29,  6:15, PM, Randy Blythe w/ Riki \n\
 ST, Oct, 29,  7:45, PM, Oct, 29,  8:45, PM, Chimaira \n\
 ST, Oct, 29, 11:00, PM, Oct, 30, 12:00, AM, Carcass \n\
 AT, Oct, 29,  2:45, PM, Oct, 29,  3:45, PM, UnityTX \n\
-AT, Oct, 29,  5:15, PM, Oct, 29,  6:15, PM, Corrosion of Conformity \n\
-AT, Oct, 29,  7:15, PM, Oct, 29,  8:45, PM, Battle Jacket Showdown \n\
+AT, Oct, 29,  5:15, PM, Oct, 29,  6:15, PM, C.O.C. \n\
+AT, Oct, 29,  7:15, PM, Oct, 29,  8:45, PM, Battle Jacket Show- down \n\
 AT, Oct, 30, 12:00, AM, Oct, 30,  1:00, AM, Exodus \n\
 SP, Oct, 29,  9:30, AM, Oct, 29, 10:30, AM, Coffee with Willie \n\
 SP, Oct, 29,  1:30, PM, Oct, 29,  2:30, PM, Mark Morton w/ Riki \n\
 SP, Oct, 29,  4:30, PM, Oct, 29,  5:30, PM, Trivia w/Jose \n\
-SC, Oct, 29,  9:00, AM, Oct, 29, 10:00, AM, Mosh Pit Bootcamp \n\
+SC, Oct, 29,  9:00, AM, Oct, 29, 10:00, AM, Mosh Pit Boot- camp \n\
 SC, Oct, 29, 10:30, AM, Oct, 29, 11:30, AM, Yoga \n\
 SC, Oct, 29,  5:15, PM, Oct, 29,  6:15, PM, Unearth \n\
 SC, Oct, 29,  7:45, PM, Oct, 29,  8:45, PM, After the Burial \n\
-SC, Oct, 29, 11:00, PM, Oct, 30, 12:00, AM, Eyehategod \
+SC, Oct, 29, 11:00, PM, Oct, 30, 12:00, AM, EHG \
 "
 ,
 "\
@@ -88,7 +88,7 @@ ST, Oct, 30,  6:15, PM, Oct, 30,  7:15, PM, Exodus \n\
 ST, Oct, 30,  8:45, PM, Oct, 30,  9:45, PM, Dethklok\n\
 ST, Oct, 30, 11:30, PM, Oct, 31, 12:30, AM, Carcass\n\
 AT, Oct, 30,  6:30, PM, Oct, 30,  7:30, PM, UnityTX\n\
-AT, Oct, 30,  8:45, PM, Oct, 30,  9:45, PM, Eyehategod\n\
+AT, Oct, 30,  8:45, PM, Oct, 30,  9:45, PM, EHG\n\
 AT, Oct, 31, 12:45, AM, Oct, 31,  1:45, AM, Poison the Well\n\
 SP, Oct, 30,  7:15, PM, Oct, 30,  8:15, PM, OFIT Galley w/ Riki\n\
 SP, Oct, 30, 11:30, PM, Oct, 31, 12:30, AM, Comedy w/ Smith/ Abrusci\n\
@@ -100,21 +100,21 @@ SC, Oct, 30, 11:30, PM, Oct, 31, 12:30, AM, Napalm Death\
 "\
 PO, Oct, 31, 10:00, AM, Oct, 31, 11:00, AM, Yoga\n\
 PO, Oct, 31,  1:45, PM, Oct, 31,  2:45, PM, After the Burial\n\
-PO, Oct, 31,  4:15, PM, Oct, 31,  5:15, PM, Corrosion of Conformity\n\
+PO, Oct, 31,  4:15, PM, Oct, 31,  5:15, PM, C.O.C.\n\
 PO, Oct, 31,  6:45, PM, Oct, 31,  7:45, PM, Poison the Well\n\
 PO, Oct, 31,  9:15, PM, Oct, 31, 10:15, PM, Carcass\n\
 ST, Oct, 31, 12:45, PM, Oct, 31,  1:45, PM, Zetro's Podcast\n\
 ST, Oct, 31,  5:30, PM, Oct, 31,  6:30, PM, Liquid Metal w/ Jose Mangin\n\
 ST, Oct, 31,  8:00, PM, Oct, 31,  9:00, PM, Dethklok\n\
 ST, Oct, 31, 10:30, PM, Oct, 31, 11:30, PM, Chimaira\n\
-AT, Oct, 31,  2:30, PM, Oct, 31,  3:30, PM, Eyehategod\n\
+AT, Oct, 31,  2:30, PM, Oct, 31,  3:30, PM, EHG\n\
 AT, Oct, 31,  4:45, PM, Oct, 31,  5:45, PM, Currents\n\
 AT, Oct, 31,  6:45, PM, Oct, 31,  7:45, PM, Costume Contest\n\
 AT, Oct, 31,  8:45, PM, Oct, 31,  9:45, PM, Bleeding Through\n\
 AT, Oct, 31, 10:30, PM, Oct, 31, 11:30, PM, Mark Morton Solo Band\n\
 AT, Nov,  1, 12:30, AM, Nov,  1,  1:30, AM, DJ Art Cruz\n\
-SC, Oct, 31,  9:00, AM, Oct, 31, 10:00, AM, Mosh Bootcamp\n\
-SC, Oct, 31,  3:00, PM, Oct, 31,  4:00, PM, Battleshots Tournament\n\
+SC, Oct, 31,  9:00, AM, Oct, 31, 10:00, AM, Mosh Pit Boot- camp\n\
+SC, Oct, 31,  3:00, PM, Oct, 31,  4:00, PM, Battle shots Tourn- ament\n\
 SC, Oct, 31,  5:30, PM, Oct, 31,  6:30, PM, UnityTX\n\
 SC, Oct, 31,  8:00, PM, Oct, 31,  9:00, PM, Frozen Soul\n\
 SC, Oct, 31, 11:00, PM, Nov,  1,  1:30, AM, Karaoke at Bliss (not at SC)\
@@ -130,10 +130,10 @@ export let bandInfo = [
     ["After the Burial", "prog"],
     ["Bleeding Through", "core"],
     ["Chimaira", "groove core"],
-    ["Corrosion of Conformity", "sludge southern"],
+    ["C.O.C.", "sludge southern"],
     ["Currents", "core"],
     ["Exodus", "thrash"],
-    ["Eyehategod", "doom"],
+    ["EHG", "doom"],
     ["Frozen Soul", "death"],
     ["Napalm Death", "punk death"],
     ["Poison the Well", "core"],
@@ -142,27 +142,26 @@ export let bandInfo = [
     ["UnityTX", "nu"],
     ["Lamb of God", "core"],
     ["Dethklok", "death"],
-    ["Toast/Photo", "other"],
+    ["Toast/ Photo", "other"],
     ["Yoga", "other"],
-    ["Battle Jacket Showdown", "other"],
-    ["Carcass", "grind/death"],
+    ["Battle Jacket Show- down", "other"],
+    ["Carcass", "grind death"],
     ["Coffee with Willie", "other"],
     ["Drum-off w/ Art Cruz", "other"],
     ["Mark Morton w/ Riki", "other"],
-    ["Mosh Pit Bootcamp", "other"],
     ["Randy Blythe w/ Riki", "other"],
     ["Trivia w/Jose", "other"],
     ["Comedy w/ Smith/ Abrusci", "comedy"],
     ["OFIT Galley w/ Riki", "other"],
     ["Puerta Plata", "other"],
-    ["Battleshots Tournament", "other"],
+    ["Battle shots Tourn- ament", "other"],
     ["Costume Contest", "other"],
     ["DJ Art Cruz", "other"],
     ["Karaoke at Bliss (not at SC)", "other"],
     ["Liquid Metal w/ Jose Mangin", "other"],
-    ["Mosh Bootcamp", "other"],
+    ["Mosh Pit Boot- camp", "other"],
     ["Zetro's Podcast", "other"],
-    ["Bingo w/ Riki/Leah", "other"],
+    ["Bingo w/ Riki/ Leah", "other"],
     ["Mark Morton Solo Band", "rock"]
 ];
 
