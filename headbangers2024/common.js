3fce3ccee43a00120ed2a78329dc1125041e95f3
alsauce <2998687+alsauce@users.noreply.github.com>
2024-08-15 22:58:42 -0500

    hbb2024 data

--- headbangers2024/common.js
+++ headbangers2024/common.js
@@ -1,6 +1,5 @@
 //Current time will be shown if it is newer than the storedTime.  Uncomment the old time when testing is done.
 //export let storedTime = getDateTime("2000", "Oct", "29", "12:02", "AM");
-
 //Set this to see current time on the schedule wherever for testing
 export let storedTime = getDateTime("2024", "Oct", "30", "10:04", "PM");
 
@@ -13,15 +12,9 @@ export let locations = [
     },
     {
         abbrev: "ST",
-        fullName: "Stardust Theater",
+        fullName: "Stardust",
         location: "6,7 fwd",
         note: "test 2"
-    },
-    {
-        abbrev: "SP",
-        fullName: "Spinnaker Lounge",
-        location: "13 fwd",
-        note: ""
     },
     {
         abbrev: "AT",
@@ -30,9 +23,15 @@ export let locations = [
         note: ""
     },
     {
-        abbrev: "MA",
-        fullName: "Magnums",
-        location: "6 mid",
+        abbrev: "SP",
+        fullName: "Spinnaker",
+        location: "13 fwd",
+        note: ""
+    },
+    {
+        abbrev: "SC",
+        fullName: "Sports Court",
+        location: "Deck 13 aft",
         note: ""
     }
 ];
@@ -41,33 +40,73 @@ export let year = 2024;
 
 export let schedule = [
 "\
-PO, Oct, 28, 6:00, PM, Oct, 28, 6:45, PM, xtest11 \n\
-ST, Oct, 28, 6:10, PM, Oct, 28, 6:45, PM, xtest13 NoGenre \n\
-SP, Oct, 28, 6:30, PM, Oct, 28, 6:55, PM, xtest12 \n\
-AT, Oct, 28, 7:10, PM, Oct, 28, 7:45, PM, xtest24 \n\
-MA, Oct, 28, 7:00, PM, Oct, 28, 7:45, PM, xtest25 \n\
-PO, Oct, 28, 8:15, PM, Oct, 28, 9:15, PM, At the Gates \n\
-PO, Oct, 29, 12:00, AM, Oct, 29, 12:30, AM, xtest66 \
+PO, Oct, 28,  5:30, PM, Oct, 28,  6:00, PM, Toast/Photo \n\
+PO, Oct, 28,  6:00, PM, Oct, 28,  7:00, PM, Exodus \n\
+PO, Oct, 28,  8:30, PM, Oct, 28,  9:30, PM, Dethklok \n\
+PO, Oct, 28, 11:00, PM, Oct, 29, 12:00, AM, Chimaria \n\
+ST, Oct, 28,  7:15, PM, Oct, 28,  8:15, PM, Napalm Death \n\
+ST, Oct, 28,  9:45, PM, Oct, 28, 10:45, PM, Soulfly \n\
+ST, Oct, 29, 12:15, AM, Oct, 29,  1:15, AM, Poison the Well \n\
+AT, Oct, 28,  3:00, PM, Oct, 28,  4:00, PM, Corrosion of Conformity \n\
+AT, Oct, 28,  7:15, PM, Oct, 28,  8:15, PM, Bleeding Through \n\
+AT, Oct, 28,  9:45, PM, Oct, 28, 10:45, PM, Frozen Soul \n\
+AT, Oct, 29,  1:15, AM, Oct, 29,  2:15, AM, After the Burial \n\
+SP, Oct, 29, 12:15, AM, Oct, 29,  1:15, AM, Comedy \n\
+SC, Oct, 28,  7:15, PM, Oct, 28,  8:15, PM, Currents \n\
+SC, Oct, 28,  9:45, PM, Oct, 28, 10:45, PM, Unearth \
 "
 ,
 "\
-PO, Oct, 29, 5:00, PM, Oct, 29, 6:45, PM, xtest11 \n\
-PO, Oct, 29, 11:30, PM, Oct, 30, 12:30, AM, xtest12 \n\
-ST, Oct, 29, 11:30, PM, Oct, 30, 12:30, AM, xtest24 \n\
-SP, Oct, 29, 11:30, PM, Oct, 30, 12:30, AM, xtest25 \n\
-AT, Oct, 29, 11:30, PM, Oct, 30, 12:30, AM, xtest13 NoGenre \n\
-AT, Oct, 29,  9:30, PM, Oct, 29, 10:30, PM, xtest13 ONE \n\
-MA, Oct, 29, 11:30, PM, Oct, 30, 12:30, AM, xtest66 \
+PO, Oct, 29,  4:30, PM, Oct, 29,  5:30, PM, Napalm Death \n\
+PO, Oct, 29,  6:45, PM, Oct, 29,  7:45, PM, Bleeding Through \n\
+PO, Oct, 29,  9:30, PM, Oct, 29, 10:45, PM, Lamb of God \n\
+ST, Oct, 29, 11:30, AM, Oct, 29,  12:30, PM, Bingo w/ Riki/Leah \n\
+ST, Oct, 29,  2:45, PM, Oct, 29,  3:45, PM, Drum-off w/ Art Cruz \n\
+ST, Oct, 29,  5:15, PM, Oct, 29,  6:15, PM, Randy Blythe w/ Riki \n\
+ST, Oct, 29,  7:45, PM, Oct, 29,  8:45, PM, Chimaira \n\
+ST, Oct, 29, 11:00, PM, Oct, 30, 12:00, AM, Carcass \n\
+AT, Oct, 29,  2:45, PM, Oct, 29,  3:45, PM, UnityTX \n\
+AT, Oct, 29,  5:15, PM, Oct, 29,  6:15, PM, Corrosion of Conformity \n\
+AT, Oct, 29,  7:15, PM, Oct, 29,  8:45, PM, Battle Jacket Showdown \n\
+AT, Oct, 30, 12:00, AM, Oct, 30,  1:00, AM, Exodus \n\
+SP, Oct, 29,  9:30, AM, Oct, 29, 10:30, AM, Coffee with Willie \n\
+SP, Oct, 29,  1:30, PM, Oct, 29,  2:30, PM, Mark Morton w/ Riki \n\
+SP, Oct, 29,  4:30, PM, Oct, 29,  5:30, PM, Trivia w/Jose \n\
+SC, Oct, 29,  9:00, AM, Oct, 29, 10:00, AM, Mosh Pit Bootcamp \n\
+SC, Oct, 29, 10:30, AM, Oct, 29, 11:30, AM, Yoga \n\
+SC, Oct, 29,  5:15, PM, Oct, 29,  6:15, PM, Unearth \n\
+SC, Oct, 29,  7:45, PM, Oct, 29,  8:45, PM, After the Burial \n\
+SC, Oct, 29, 11:00, PM, Oct, 30, 12:00, AM, Eyehategod \
 "
 ,
 "\
-ST, Oct, 30, 7:00, PM, Oct, 30, 8:00, PM, xtest11 \n\
-PO, Oct, 30, 11:00, PM, Oct, 31, 1:00, AM, xtest13 NoGenre \
+PO, Oct, 30,  7:00, AM, Oct, 30,  4:30, PM, Puerta Plata\n\
+PO, Oct, 30,  5:00, PM, Oct, 30,  6:00, PM, Soulfly\n\
+PO, Oct, 30,  7:15, PM, Oct, 30,  8:15, PM, Unearth\n\
+PO, Oct, 30, 10:00, PM, Oct, 30, 11:15, PM, Lamb of God\n\
+ST, Oct, 30,  6:15, PM, Oct, 30,  7:15, PM, Exodus \n\
+ST, Oct, 30,  8:45, PM, Oct, 30,  9:45, PM, Dethklok\n\
+ST, Oct, 30, 11:30, PM, Oct, 31, 12:30, AM, Carcass\n\
+AT, Oct, 30,  6:30, PM, Oct, 30,  7:30, PM, UnityTX\n\
+AT, Oct, 30,  8:45, PM, Oct, 30,  9:45, PM, Eyehategod\n\
+AT, Oct, 31, 12:45, AM, Oct, 31,  1:45, AM, Poison the Well\n\
+SP, Oct, 30,  7:15, PM, Oct, 30,  8:15, PM, OFIT Galley w/ Riki\n\
+SP, Oct, 30, 11:30, PM, Oct, 31, 12:30, AM, Comedy w/ Smith/Abrusci\n\
+SC, Oct, 30,  6:15, PM, Oct, 30,  7:15, PM, Currents\n\
+SC, Oct, 30,  8:45, PM, Oct, 30,  9:45, PM, Frozen Soul\n\
+SC, Oct, 30, 11:30, PM, Oct, 31, 12:30, AM, Napalm Death\
 "
 ,
-
-"PO, Oct, 31, 10:00, AM, Oct, 31, 10:45, AM, xtest11\n\
-PO, Oct, 31, 8:15, PM, Oct, 31, 9:15, PM, At the Gates \n\
+"\
+, Oct, 31, :, PM, Oct, 31, :, PM, \n\
+, Oct, 31, :, PM, Oct, 31, :, PM, \n\
+, Oct, 31, :, PM, Oct, 31, :, PM, \n\
+, Oct, 31, :, PM, Oct, 31, :, PM, \n\
+, Oct, 31, :, PM, Oct, 31, :, PM, \n\
+, Oct, 31, :, PM, Oct, 31, :, PM, \n\
+, Oct, 31, :, PM, Oct, 31, :, PM, \n\
+, Oct, 31, :, PM, Oct, 31, :, PM, \n\
+, Oct, 31, :, PM, Oct, 31, :, PM, \n\
 "
 ];
 
