aee88c1f462dfb16083161269fe94fcf35f6a25f
alsauce <alsauce>
2024-10-28 18:26:03 -0400

    update common.js

--- headbangers2024/common.js
+++ headbangers2024/common.js
@@ -38,7 +38,6 @@ export let schedule = [
 "\
 PO, Oct, 28,  6:00, PM, Oct, 28,  6:15, PM, Toast/ Photo \n\
 PO, Oct, 28,  6:15, PM, Oct, 28,  7:15, PM, Exodus \n\
-PO, Oct, 28,  8:30, PM, Oct, 28,  9:30, PM, Dethklok \n\
 PO, Oct, 28, 11:00, PM, Oct, 29, 12:00, AM, Chimaira \n\
 ST, Oct, 28,  7:15, PM, Oct, 28,  8:15, PM, Napalm Death \n\
 ST, Oct, 28,  9:45, PM, Oct, 28, 10:45, PM, Soulfly \n\
@@ -46,7 +45,6 @@ ST, Oct, 29, 12:15, AM, Oct, 29,  1:15, AM, Poison the Well \n\
 AT, Oct, 28,  3:00, PM, Oct, 28,  4:00, PM, C.O.C. \n\
 AT, Oct, 28,  7:15, PM, Oct, 28,  8:15, PM, Bleeding Through \n\
 AT, Oct, 28,  9:45, PM, Oct, 28, 10:45, PM, Frozen Soul \n\
-AT, Oct, 29,  1:15, AM, Oct, 29,  2:15, AM, After the Burial \n\
 SP, Oct, 29, 12:15, AM, Oct, 29,  1:15, AM, Comedy w/ Smith/ Abrusci \n\
 SC, Oct, 28,  7:15, PM, Oct, 28,  8:15, PM, Currents \n\
 SC, Oct, 28,  9:45, PM, Oct, 28, 10:45, PM, Unearth \
@@ -72,7 +70,6 @@ SP, Oct, 29,  4:30, PM, Oct, 29,  5:30, PM, Trivia w/Jose \n\
 SC, Oct, 29,  9:00, AM, Oct, 29, 10:00, AM, Mosh Pit Boot- camp \n\
 SC, Oct, 29, 10:30, AM, Oct, 29, 11:30, AM, Yoga \n\
 SC, Oct, 29,  5:15, PM, Oct, 29,  6:15, PM, Unearth \n\
-SC, Oct, 29,  7:45, PM, Oct, 29,  8:45, PM, After the Burial \n\
 SC, Oct, 29, 11:00, PM, Oct, 30, 12:00, AM, EHG \
 "
 ,
@@ -96,7 +93,6 @@ SC, Oct, 30, 11:30, PM, Oct, 31, 12:30, AM, Napalm Death\
 ,
 "\
 PO, Oct, 31, 10:00, AM, Oct, 31, 11:00, AM, Yoga\n\
-PO, Oct, 31,  1:45, PM, Oct, 31,  2:45, PM, After the Burial\n\
 PO, Oct, 31,  4:15, PM, Oct, 31,  5:15, PM, C.O.C.\n\
 PO, Oct, 31,  6:45, PM, Oct, 31,  7:45, PM, Poison the Well\n\
 PO, Oct, 31,  9:15, PM, Oct, 31, 10:15, PM, Carcass\n\
