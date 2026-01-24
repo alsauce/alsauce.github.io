11399b13c8e6d80fb4e913a7247c91a2761694d0
alsauce <2998687+alsauce@users.noreply.github.com>
2026-01-24 01:54:44 -0600

    70k2026_1

--- 70k2026/common.js
+++ 70k2026/common.js
@@ -31,125 +31,28 @@ export let year = 2026;
 
 export let schedule = [
 "\
-PD, Oct, 31,  5:15, PM, Oct, 31,  6:15, PM, Clutch \n\
-PD, Oct, 31,  7:45, PM, Oct, 31,  8:45, PM, Nekrogoblikon \n\
-PD, Oct, 31, 10:00, PM, Oct, 31, 11:00, PM, Kublai Khan TX \n\
-SD, Oct, 31,  6:30, PM, Oct, 31,  7:30, PM, Fear Factory \n\
-SD, Oct, 31,  8:45, PM, Oct, 31,  9:45, PM, DevilDriver \n\
-SD, Oct, 31, 11:30, PM, Nov,  1, 12:30, AM, Obituary \n\
-BL, Oct, 31,  3:00, PM, Oct, 31,  4:00, PM, Crowbar \n\
-BL, Oct, 31,  8:30, PM, Oct, 31,  9:30, PM, Black Dhalia \n\
-BL, Oct, 31, 11:00, PM, Nov,  1, 12:00, AM, Exploited \n\
-MA, Oct, 31,  6:30, PM, Oct, 31,  7:30, PM, Brat \n\
-MA, Oct, 31,  9:00, PM, Oct, 31, 10:00, PM, Walls of Jericho \n\
-MA, Oct, 31, 11:00, PM, Nov,  1,  1:00, AM, Karaoke \n\
-SP, Oct, 31,  2:00, PM, Oct, 31,  3:00, PM, Wellness \n\
-SP, Oct, 31,  6:00, PM, Oct, 31,  8:00, PM, Swag Swap \n\
-SP, Oct, 31, 11:00, PM, Nov,  1, 12:00, AM, Comedy \
+PD, Oct, 31,  5:15, PM, Oct, 31,  6:15, PM, test1 \n\
+RT, Oct, 31, 11:00, PM, Nov,  1, 12:00, AM, test1 \
 "
 ,
 "\
-PD, Nov,  1,  3:30, PM, Nov,  1,  4:30, PM, Fear Factory \n\
-PD, Nov,  1,  6:30, PM, Nov,  1,  7:30, PM, Obituary \n\
-PD, Nov,  1,  9:30, PM, Nov,  1, 10:45, PM, Lamb of God \n\
-SD, Nov,  1, 11:00, AM, Nov,  1, 12:00, PM, Defender ot Riff \n\
-SD, Nov,  1,  1:00, PM, Nov,  1,  2:00, PM, Guitar-off \n\
-SD, Nov,  1,  5:00, PM, Nov,  1,  6:30, PM, Blythe Spoken Word \n\
-SD, Nov,  1,  8:00, PM, Nov,  1,  9:00, PM, Power Trip \n\
-SD, Nov,  1, 11:15, PM, Nov,  2, 12:15, AM, Black Dhalia \n\
-BL, Nov,  1,  2:30, PM, Nov,  1,  3:30, PM, Walls of Jericho \n\
-BL, Nov,  1,  7:30, PM, Nov,  1,  8:30, PM, Gideon \n\
-BL, Nov,  1, 11:00, PM, Nov,  2, 12:00, AM, Nekrogoblikon \n\
-BL, Nov,  2, 12:45, AM, Nov,  2,  2:00, AM, DJ Art Cruz \n\
-MA, Nov,  1, 12:00, PM, Nov,  1,  2:00, PM, Pit of Sound \n\
-MA, Nov,  1,  4:30, PM, Nov,  1,  5:30, PM, Crowbar \n\
-MA, Nov,  1,  8:00, PM, Nov,  1,  9:00, PM, Brat \n\
-MA, Nov,  1, 12:00, PM, Nov,  1,  2:00, PM, Pit of Sound \n\
-MA, Nov,  2, 12:00, AM, Nov,  2,  1:00, AM, Eighteen Visions \n\
-SP, Nov,  1,  9:00, AM, Nov,  1, 10:30, AM, Coffee with Willie \n\
-SP, Nov,  1, 12:00, PM, Nov,  1,  1:00, PM, Bingo \n\
-SP, Nov,  1,  2:30, PM, Nov,  1,  3:30, PM, Self Care Panel \n\
-SP, Nov,  1,  5:30, PM, Nov,  1,  6:30, PM, Trivia \
+PD, Nov,  1,  3:30, PM, Nov,  1,  4:30, PM, test1 \n\
+SB, Nov,  1,  5:30, PM, Nov,  1,  6:30, PM, test1 \
 "
 ,
 "\
-PD, Nov,  2,  9:00, AM, Nov,  2,  4:00, PM, Cozumel \n\
-PD, Nov,  2,  5:00, PM, Nov,  2,  6:15, PM, Lamb of God \n\
-PD, Nov,  2,  8:30, PM, Nov,  2,  9:30, PM, Kublai Khan TX \n\
-PD, Nov,  2, 11:00, PM, Nov,  3, 12:00, AM, Clutch \n\
-SD, Nov,  2,  7:00, PM, Nov,  2,  8:00, PM, Exploited \n\
-SD, Nov,  2,  9:45, PM, Nov,  2, 10:45, PM, DevilDriver \n\
-SD, Nov,  3, 12:30, AM, Nov,  3,  1:30, AM, Crowbar \n\
-BL, Nov,  2,  7:15, PM, Nov,  2,  8:15, PM, Brat \n\
-BL, Nov,  3, 12:00, AM, Nov,  3,  1:00, AM, Category7 \n\
-MA, Nov,  2,  7:00, PM, Nov,  2,  8:00, PM, Walls of Jericho \n\
-MA, Nov,  2,  9:30, PM, Nov,  2, 10:30, PM, Eighteen Visions \n\
-MA, Nov,  3, 12:15, AM, Nov,  3,  1:15, AM, Gideon \n\
-SP, Nov,  2, 10:00, PM, Nov,  2, 11:00, PM, Comedy \
+PD, Nov,  2,  9:00, AM, Nov,  2,  4:00, PM, test1 \n\
+SL, Nov,  2, 10:00, PM, Nov,  2, 11:00, PM, test1 \
 "
 ,
 "\
-PD, Nov,  3,  4:30, PM, Nov,  3,  5:30, PM, Black Dhalia \n\
-PD, Nov,  3,  7:30, PM, Nov,  3,  8:30, PM, DevilDriver \n\
-PD, Nov,  3, 10:00, PM, Nov,  3, 11:00, PM, Power Trip \n\
-SD, Nov,  3, 11:00, AM, Nov,  3, 12:00, PM, Defender ot Riff \n\
-SD, Nov,  3,  3:15, PM, Nov,  3,  4:15, PM, Obituary \n\
-SD, Nov,  3,  6:00, PM, Nov,  3,  7:00, PM, HBB w/ Category7 \n\
-SD, Nov,  3,  8:30, PM, Nov,  3,  9:30, PM, Fear Factory \n\
-SD, Nov,  3, 11:30, PM, Nov,  4, 12:30, AM, Clutch \n\
-BL, Nov,  3,  1:30, PM, Nov,  3,  2:30, PM, Eighteen Visions \n\
-BL, Nov,  3,  3:30, PM, Nov,  3,  4:30, PM, Nekrogoblikon \n\
-BL, Nov,  3,  6:30, PM, Nov,  3,  7:30, PM, Battle Jacket SD \n\
-BL, Nov,  3,  8:45, PM, Nov,  3,  9:45, PM, Mark Morton \n\
-BL, Nov,  3, 11:00, PM, Nov,  4, 12:00, AM, Kublai Khan TX \n\
-MA, Nov,  3, 12:00, PM, Nov,  3,  2:00, PM, Pit of Sound \n\
-MA, Nov,  3,  3:30, PM, Nov,  3,  5:00, PM, Battleshots \n\
-MA, Nov,  3,  6:15, PM, Nov,  3,  7:15, PM, Exploited \n\
-MA, Nov,  3,  8:15, PM, Nov,  3,  9:15, PM, Gideon \n\
-MA, Nov,  3, 11:00, PM, Nov,  4,  1:00, AM, Karaoke \n\
-SP, Nov,  3, 12:00, PM, Nov,  3,  1:00, PM, QA Riki Rachtman \n\
-SP, Nov,  3,  2:00, PM, Nov,  3,  3:00, PM, Screaming \
+PD, Nov,  3,  4:30, PM, Nov,  3,  5:30, PM, test1 \n\
+SL, Nov,  3,  2:00, PM, Nov,  3,  3:00, PM, test1 \
 "
 ];
 
 
 export let bandInfo = [
-    ["Battle Jacket SD", "other", ""],
-    ["Battleshots", "other", ""],
-    ["HBB w/ Category7", "other", ""],
-    ["Bingo", "other", ""],
-    ["Black Dhalia", "death", ""],
-    ["Blythe Spoken Word", "other", ""],
-    ["Brat", "grind", ""],
-    ["Category7", "thrash", ""],
-    ["Clutch", "rock", ""],
-    ["Coffee with Willie", "other", ""],
-    ["Comedy", "other", ""],
-    ["Cozumel", "other", ""],
-    ["Crowbar", "sludge", ""],
-    ["Defender ot Riff", "other", ""],
-    ["DevilDriver", "heavy", ""],
-    ["DJ Art Cruz", "other", ""],
-    ["Eighteen Visions", "punk post", ""],
-    ["Exploited", "punk thrash", ""],
-    ["Fear Factory", "industrial", ""],
-    ["Gideon", "thrash", ""],
-    ["Guitar-off", "other", ""],
-    ["Karaoke", "other", ""],
-    ["Kublai Khan TX", "beatdown hard", ""],
-    ["Lamb of God", "groove", ""],
-    ["Mark Morton", "rock", ""],
-    ["Nekrogoblikon", "melo folk", ""],
-    ["Obituary", "death", ""],
-    ["Pit of Sound", "other", ""],
-    ["Power Trip", "thrash", ""],
-    ["QA Riki Rachtman", "other", ""],
-    ["Screaming", "other", ""],
-    ["Self Care Panel", "other", ""],
-    ["Swag Swap", "other", ""],
-    ["Trivia", "Other", ""],
-    ["Walls of Jericho", "punk thrash", ""],
-    ["Wellness", "other", ""]
 ];
 
 export let storedVersion = "_70k2026_v1";
