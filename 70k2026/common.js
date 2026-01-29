222623558b8c9dd795ddd8280834dabc437063fb
Allen Olsen <allen.olsen@oracle.com>
2026-01-28 23:14:57 -0600

    running order

--- 70k2026/common.js
+++ 70k2026/common.js
@@ -31,28 +31,218 @@ export let year = 2026;
 
 export let schedule = [
 "\
-PD, Jan, 29,  5:15, PM, Jan, 29,  6:15, PM, test1 \n\
-RT, Jan, 29, 11:00, PM, Jan, 30, 12:00, AM, test1 \
+RT, Jan, 29,  6:15, PM, Jan, 29,  7:00, PM, Xandria \n\
+RT, Jan, 29,  7:45, PM, Jan, 29,  8:30, PM, Soen \n\
+RT, Jan, 29,  9:15, PM, Jan, 29, 10:15, PM, Kamelot \n\
+RT, Jan, 29, 11:15, PM, Jan, 30, 12:15, AM, Anthrax \n\
+RT, Jan, 30,  1:15, AM, Jan, 30,  2:15, AM, Amorphis \n\
+RT, Jan, 30,  3:00, AM, Jan, 30,  3:45, AM, Leaves' Eyes \n\
+RT, Jan, 30,  4:30, AM, Jan, 30,  5:15, AM, Dust Bolt \n\
+SB, Jan, 29,  5:30, PM, Jan, 29,  6:15, PM, Vio-Lence \n\
+SB, Jan, 29,  7:00, PM, Jan, 29,  7:45, PM, Harakiri for the Sky \n\
+SB, Jan, 29,  8:30, PM, Jan, 29,  9:15, PM, Cemetary Skyline \n\
+SB, Jan, 29, 10:15, PM, Jan, 29, 11:00, PM, TYR \n\
+SB, Jan, 30, 12:30, AM, Jan, 30,  1:15, AM, Kanonenfieber \n\
+SB, Jan, 30,  3:45, AM, Jan, 30,  3:00, AM, Skyclad \n\
+SB, Jan, 30,  3:45, AM, Jan, 30,  4:30, AM, Suikakra \n\
+SB, Jan, 30,  5:15, AM, Jan, 30,  6:00, AM, Vitam Et Mortem \n\
+SL, Jan, 29,  5:30, PM, Jan, 29,  6:15, PM, Seven Spires \n\
+SL, Jan, 29,  7:00, PM, Jan, 29,  7:45, PM, Srike Master \n\
+SL, Jan, 29,  8:30, PM, Jan, 29,  9:15, PM, Gama Bomb \n\
+SL, Jan, 29, 10:30, PM, Jan, 29, 11:15, PM, Dragonland \n\
+SL, Jan, 30, 12:30, AM, Jan, 30,  1:15, AM, Illumishade \n\
+SL, Jan, 30,  2:15, AM, Jan, 30,  3:00, AM, Trash Panda \n\
+PD, Jan, 30, 12:00, AM, Jan, 30,  6:00, AM, Karaoke AT PLAYERMAKERS \
 "
 ,
 "\
-PD, Jan, 30,  3:30, PM, Jan, 30,  4:30, PM, test1 \n\
-SB, Jan, 30,  5:30, PM, Jan, 30,  6:30, PM, test1 \
+PD, Jan, 30,  9:00, AM, Jan, 30,  4:00, PM, Nassau \n\
+PD, Jan, 30,  5:30, PM, Jan, 30,  6:16, PM, Orden Ogan \n\
+PD, Jan, 30,  7:00, PM, Jan, 30,  8:00, PM, Amorphis \n\
+PD, Jan, 30,  8:45, PM, Jan, 30,  9:45, PM, Beast in Black \n\
+PD, Jan, 30, 10:45, PM, Jan, 30, 11:45, PM, Soilwork \n\
+PD, Jan, 31, 12:30, AM, Jan, 31,  1:15, AM, Kanonenfieber \n\
+PD, Jan, 31,  2:00, AM, Jan, 31,  2:45, AM, Leaves' Eyes \n\
+PD, Jan, 31,  3:30, AM, Jan, 31,  4:15, AM, Vader \n\
+PD, Jan, 31,  5:00, AM, Jan, 31,  5:45, AM, Bloodred Hourglass \n\
+RT, Jan, 30,  6:15, PM, Jan, 30,  7:00, PM, Ad Infinitum \n\
+RT, Jan, 30,  8:00, PM, Jan, 30,  8:45, PM, Harakiri for the Sky \n\
+RT, Jan, 30,  9:45, PM, Jan, 30, 10:45, PM, Haggard \n\
+RT, Jan, 30, 11:45, PM, Jan, 31, 12:30, AM, Firewind \n\
+RT, Jan, 31,  1:15, AM, Jan, 31,  2:00, AM, Cemetary Skyline \n\
+RT, Jan, 31,  2:45, AM, Jan, 31,  3:30, AM, Skyclad \n\
+SB, Jan, 30,  5:30, PM, Jan, 30,  6:15, PM, Izegrim \n\
+SB, Jan, 30,  7:15, PM, Jan, 30,  8:00, PM, Ignea \n\
+SB, Jan, 30,  8:45, PM, Jan, 30,  9:30, PM, Ereb Altor \n\
+SB, Jan, 30, 10:15, PM, Jan, 30, 11:00, PM, Royal Hunt \n\
+SB, Jan, 30, 11:45, PM, Jan, 31, 12:30, AM, Illumishade \n\
+SB, Feb, 31,  1:15, AM, Jan, 31,  2:00, AM, Saturnus \n\
+SB, Jan, 31,  2:45, AM, Jan, 31,  3:30, AM, Seven Spires \n\
+SB, Jan, 31,  4:15, AM, Jan, 31,  5:00, AM, T.H.E.M \n\
+SL, Jan, 30,  6:15, PM, Jan, 30,  7:00, PM, Persefone \n\
+SL, Jan, 30,  8:00, PM, Jan, 30,  8:45, PM, Wolf \n\
+SL, Jan, 30,  9:30, PM, Jan, 30, 10:15, PM, Skeletal Remains \n\
+SL, Jan, 30, 11:00, PM, Jan, 30, 11:45, PM, Jag Panzer \n\
+SL, Jan, 31, 12:30, AM, Jan, 31,  1:15, AM, In Mourning \n\
+SL, Jan, 31,  2:00, AM, Jan, 31,  2:45, AM, Vitam Et Mortem \
 "
 ,
 "\
-PD, Jan, 31,  9:00, AM, Jan, 31,  4:00, PM, test1 \n\
-SL, Jan, 31, 10:00, PM, Jan, 31, 11:00, PM, test1 \
+PD, Jan, 31, 10:00, AM, Jan, 31, 10:45, AM, Hiraes \n\
+PD, Jan, 31, 11:30, AM, Jan, 31, 12:15, PM, VBO \n\
+PD, Jan, 31,  1:00, PM, Jan, 31,  1:45, PM, Ignea \n\
+PD, Jan, 31,  2:30, PM, Jan, 31,  3:15, PM, Ad Infinitum \n\
+PD, Jan, 31,  4:00, PM, Jan, 31,  4:45, PM, Firewind \n\
+PD, Jan, 31,  5:45, PM, Jan, 31,  6:45, PM, Haggard \n\
+PD, Jan, 31,  7:45, PM, Jan, 31,  8:45, PM, Paradise Lost \n\
+PD, Jan, 31,  9:45, PM, Jan, 31, 11:00, PM, Anthrax \n\
+PD, Feb,  1, 12:00, AM, Feb,  1,  1:00, AM, Wind Rose \n\
+PD, Feb,  1,  2:00, AM, Feb,  1,  2:45, AM, Arkona \n\
+PD, Feb,  1,  3:30, AM, Feb,  1,  4:15, AM, Saturnus \n\
+PD, Feb,  1,  5:00, AM, Feb,  1,  5:45, AM, Izegrim \n\
+RT, Jan, 31, 10:45, AM, Jan, 31, 11:30, AM, Darkane \n\
+RT, Jan, 31, 12:15, PM, Jan, 31,  1:00, PM, Heathen \n\
+RT, Jan, 31,  1:45, PM, Jan, 31,  2:30, PM, Royal Hunt \n\
+RT, Jan, 31,  3:15, PM, Jan, 31,  4:00, PM, Orden Ogan \n\
+RT, Jan, 31,  4:45, PM, Jan, 31,  5:45, PM, Soilwork \n\
+RT, Jan, 31,  6:45, PM, Jan, 31,  7:45, PM, Beast in Black \n\
+RT, Jan, 31,  8:45, PM, Jan, 31,  9:45, PM, Dark Tranquility \n\
+RT, Jan, 31, 11:00, PM, Feb,  1, 12:00, AM, Eluveitie \n\
+RT, Feb,  1,  1:00, AM, Feb,  1,  2:00, AM, Insomnium \n\
+RT, Feb,  1,  2:45, AM, Feb,  1,  3:30, AM, Vader \n\
+SB, Jan, 31, 11:30, AM, Jan, 31, 12:15, PM, Wolf \n\
+SB, Jan, 31,  1:00, PM, Jan, 31,  1:45, PM, Minipony \n\
+SB, Jan, 31,  2:30, PM, Jan, 31,  3:15, PM, Satan \n\
+SB, Jan, 31,  4:00, PM, Jan, 31,  4:45, PM, In Mourning \n\
+SB, Jan, 31,  5:30, PM, Jan, 31,  6:15, PM, Tribulation \n\
+SB, Jan, 31,  7:00, PM, Jan, 31,  7:45, PM, Jag Panzer \n\
+SB, Jan, 31,  8:30, PM, Jan, 31,  8:15, PM, Trick or Treat \n\
+SB, Jan, 31, 10:00, PM, Jan, 31, 10:45, PM, Persefone \n\
+SB, Jan, 31, 11:45, PM, Feb,  1, 12:30, AM, Bloodred Hourglass \n\
+SB, Feb,  1,  1:15, AM, Feb,  1,  2:00, AM, Dodheimsgard \n\
+SB, Feb,  1,  2:45, AM, Feb,  1,  3:30, AM, Rhapsody of Fire \n\
+SB, Feb,  1,  4:15, AM, Feb,  1,  5:00, AM, Skeletal Remains \n\
+SL, Jan, 31,  3:15, PM, Jan, 31,  4:00, PM, Ereb Altor \n\
+SL, Jan, 31,  4:45, PM, Jan, 31,  5:30, PM, T.H.E.M \n\
+SL, Jan, 31,  6:15, PM, Jan, 31,  7:00, PM, Illdisposed \n\
+SL, Jan, 31,  7:45, PM, Jan, 31,  8:30, PM, Hour of Penance \n\
+SL, Jan, 31,  9:15, PM, Jan, 31, 10:00, PM, In Virtue \n\
+SL, Jan, 31, 11:00, PM, Jan, 31, 11:45, PM, Groza \n\
+SL, Feb,  1, 12:30, AM, Feb,  1,  1:15, AM, Hirax \
 "
 ,
 "\
-PD, Feb,  1,  4:30, PM, Feb,  1,  5:30, PM, test1 \n\
-SL, Feb,  1,  2:00, PM, Feb,  1,  3:00, PM, test1 \
+PD, Feb,  1, 10:00, AM, Feb,  1, 10:45, AM, Xandria \n\
+PD, Feb,  1, 11:30, AM, Feb,  1, 12:15, PM, TYR \n\
+PD, Feb,  1, 12:15, PM, Feb,  1,  1:15, PM, Bellyflop \n\
+PD, Feb,  1,  1:15, PM, Feb,  1,  2:00, PM, Darkane \n\
+PD, Feb,  1,  2:45, PM, Feb,  1,  3:30, PM, Hirax \n\
+PD, Feb,  1,  4:15, PM, Feb,  1,  5:00, PM, Soen \n\
+PD, Feb,  1,  5:45, PM, Feb,  1,  6:45, PM, Insomnium \n\
+PD, Feb,  1,  7:30, PM, Feb,  1,  8:30, PM, Kamelot \n\
+PD, Feb,  1,  9:30, PM, Feb,  1, 10:30, PM, Eluveitie \n\
+PD, Feb,  1, 11:30, PM, Feb,  2, 12:30, AM, Dark Tranquility \n\
+PD, Feb,  2, 12:30, AM, Feb,  2,  1:00, AM, Skipper's TY \n\
+PD, Feb,  2,  1:00, AM, Feb,  2,  4:00, AM, Karaoke at Solarium \n\
+RT, Feb,  1, 10:45, AM, Feb,  1, 11:30, AM, Dragonland \n\
+RT, Feb,  1, 12:15, PM, Feb,  1,  1:00, PM, Satan \n\
+RT, Feb,  1,  2:30, PM, Feb,  1,  4:00, PM, All Star Jam \n\
+RT, Feb,  1,  5:00, PM, Feb,  1,  5:45, PM, Arkona \n\
+RT, Feb,  1,  6:45, PM, Feb,  1,  7:30, PM, Rhopsody of Fire \n\
+RT, Feb,  1,  8:30, PM, Feb,  1,  9:30, PM, Paradise Lost \n\
+RT, Feb,  1, 10:30, PM, Feb,  1, 11:30, PM, Wind Rose \n\
+SB, Feb,  1, 10:45, AM, Feb,  1, 11:30, AM, In Virtue \n\
+SB, Feb,  1, 12:15, PM, Feb,  1,  1:00, PM, Strike Master \n\
+SB, Feb,  1,  1:45, PM, Feb,  1,  2:30, PM, Groza \n\
+SB, Feb,  1,  3:30, PM, Feb,  1,  4:15, PM, VBO \n\
+SB, Feb,  1,  5:00, PM, Feb,  1,  5:45, PM, Trash Panda \n\
+SB, Feb,  1,  6:30, PM, Feb,  1,  7:15, PM, Gama Bomb \n\
+SB, Feb,  1,  8:00, PM, Feb,  1,  8:45, PM, Illdisposed \n\
+SB, Feb,  1,  9:30, PM, Feb,  1, 10:15, PM, Heathen \n\
+SB, Feb,  1, 11:00, PM, Feb,  1, 11:45, PM, Hour of Penance \n\
+SB, Feb,  2,  1:30, AM, Feb,  2,  2:15, AM, Hiraes \n\
+SL, Feb,  1,  1:00, PM, Feb,  1,  1:45, PM, Trick or Treat \n\
+SL, Feb,  1,  2:45, PM, Feb,  1,  3:30, PM, Minipony \n\
+SL, Feb,  1,  4:15, PM, Feb,  1,  5:00, PM, Dodheimsgard \n\
+SL, Feb,  1,  5:45, PM, Feb,  1,  6:30, PM, Vio-lence \n\
+SL, Feb,  1,  7:15, PM, Feb,  1,  8:00, PM, Dust Bolt \n\
+SL, Feb,  1,  8:45, PM, Feb,  1,  9:30, PM, Suidakra \n\
+SL, Feb,  1, 10:15, PM, Feb,  1, 11:00, PM, Tribulation \
 "
 ];
 
-
+//bellyflop
+//nassau
+//Skipper's TY
+//2 karaokes
+//All Star Jam
 export let bandInfo = [
+["Bellyflop", "other", ""],
+["Nassau", "other", ""],
+["Skipper's TY", "other", ""],
+["Karaoke AT PLAYERMAKERS", "other", ""],
+["Karaoke at Solarium", "other", ""],
+["All Star Jam", "other", ""],
+["Amorphis", "prog", ""],
+["Anthrax", "thrash", ""],
+["Arkona", "black", ""],
+["Beast in Black", "power", ""],
+["Bloodred Hourglass", "melo", ""],
+["Cemetary Skyline", "rock", ""],
+["Darkane", "melo", ""],
+["Dodheimsgard", "black", ""],
+["Dragonland", "power", ""],
+["Dust Bolt", "thrash", ""],
+["Eluveitie", "folk", ""],
+["Ereb Altor", "viking", ""],
+["Firewind", "power", ""],
+["Gama Bomb", "thrash", ""],
+["Groza", "black", ""],
+["Haggard", "prog", ""],
+["Harakiri for the Sky", "black", ""],
+["Heathen", "tech", ""],
+["Hiraes", "melo", ""],
+["Hirax", "thrash", ""],
+["Hour of Penance", "brutal", ""],
+["Ignea", "symph folk", ""],
+["Illdisposed", "melo", ""],
+["Illumishade", "prog", ""],
+["In Mourning", "melo", ""],
+["Insomnium", "melo", ""],
+["Izegrim", "thrash", ""],
+["Kamelot", "power", ""],
+["Kanonenfieber", "black", ""],
+["Orden Ogan", "power", ""],
+["Paradise Lost", "doom", ""],
+["Persefone", "prog", ""],
+["Rhapsody of Fire", "power", ""],
+["Royal Hunt", "power", ""],
+["Satan", "heavy", ""],
+["Saturnus", "melo", ""],
+["Seven Spires", "symph", ""],
+["Skeletal Remains", "death", ""],
+["Skyclad", "folk", ""],
+["Soen", "prog", ""],
+["Soilwork", "melo", ""],
+["Suidakra", "melo", ""],
+["Tribulation", "goth", ""],
+["Tyr", "folk", ""],
+["Vader", "thrash", ""],
+["VBO", "", ""],
+["Vio-lence", "thrash", ""],
+["Wind Rose", "folk", ""],
+["Wolf", "heavy", ""],
+["Xandria", "symph", ""],
+["Ad Infinitum", "", ""],
+["Jag Panzer", "", ""],
+["Vitam Et Mortem", "", ""],
+["T.H.E.M", "", ""],
+["In Virtue", "", ""],
+["Minipony", "", ""],
+["Strike Master", "", ""],
+["Trash Panda", "", ""],
+["Trick or Treat", "", ""],
+["Leaves' Eyes", "", ""]
 ];
 
 export let storedVersion = "_70k2026_v1";
